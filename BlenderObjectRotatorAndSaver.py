5d44a12470df71abc427408d9d4f4c644b497df2
Radzievskyi <nn5n@ukr.net>
2016-12-01 20:34:17 +0700

    BlenderObjectRotatorAndSaver.py
    Is specific script, what make selected object rotate around 360 degrees of Z axis. And result safe in separate files what can be correct import to Unity.
    
    You must set "numberOfCopies" what show how many rotation steps do you want get.
    For sample if numberOfCopies  = 2, and script will create rotation on 0 and 180 degrees;
    If numberOfCopies = 3, you will get rotation on angles : 0, 120, 240 
    
    Every rotation, script save in different blender file, and before is automatically create folder where will be kept this files.
    
    Notice:
    - Script rotate objects what will be correct open in Unity;
    - Script have sense use if your scene have only one mesh object;

---
+++ BlenderObjectRotatorAndSaver.py
@@ -0,0 +1,77 @@
+#
+# Mykhailo Radzievskyi
+#
+# Script for import objects to Unity.
+# Rotate selected object around Z axis, and save every rotation as new Blender File
+# In new folder what have name of current opened file
+#
+# Tested with Blender v2.78 and Unity 5.4.3f1 on MacOS Sierra
+#
+# 2016-11-30
+
+import bpy
+import mathutils
+import os
+from math import radians, pi
+import shutil
+
+#How many variation of rotation need create
+numberOfCopies = 4
+currentSceneName = "Scene"
+
+def RotateAndSaveSelectedObject():
+    #Get current file Path and file Name
+    filepath = bpy.data.filepath
+    directory = os.path.dirname(filepath)
+    #print(directory)
+    
+    fileName = bpy.path.basename(filepath)
+    fileName = os.path.splitext(fileName)[0]
+    
+    # Create new directory in file location folder with name of current File
+    directoryForRotation = os.path.join(directory, fileName)
+    # print(directoryForRotation)
+    shutil.os.mkdir(directoryForRotation)
+    
+    #Get current Scene
+    sce = bpy.data.scenes[currentSceneName]
+    ob = bpy.context.scene.objects[0]
+    #Checj if present Mesh object on Scene
+    if (ob.type != 'MESH'):
+        return
+    #Remember original object Name
+    baseObjectName = ob.name 
+
+    #Calculate Rotation Angle for every itteration
+    rotationIncrement = 360 / (numberOfCopies)
+    #Variable for set rotation of current Object
+    currentZRotation = 0
+    
+    for i in range(numberOfCopies):
+        # Rotate and apply rotation fixing, for correct import to Unity
+        bpy.context.object.rotation_euler = (pi / 2, 0, radians(rotationIncrement))
+        bpy.ops.object.transform_apply(location=False, rotation=True, scale=False)
+        bpy.context.object.rotation_euler = (- pi / 2, 0, 0)
+        bpy.ops.object.transform_apply(location=False, rotation=True, scale=False)
+        bpy.context.object.rotation_euler = (pi / 2, 0, 0)
+        
+        # Rename rotated object
+        bpy.context.object.name = baseObjectName + "." + str(currentZRotation)
+        # Create directory for keep rotation files
+
+        # Set name for new file (Base name + rotation angle)
+        newfile_name = os.path.join(directoryForRotation, fileName + "_" +  str(currentZRotation) + ".blend")
+        #S ave file
+        bpy.ops.wm.save_mainfile(filepath=newfile_name)
+        #C alculate next rotation angle
+        currentZRotation = currentZRotation + rotationIncrement
+    
+    # Restore original position and name of selected object    
+    bpy.context.object.rotation_euler = (radians(180) / 2, 0, radians(0))
+    bpy.context.object.name = baseObjectName
+    return
+
+# Run calculation
+RotateAndSaveSelectedObject()
+# Update scene
+bpy.context.scene.update() 
